3e5bf1eec4c8534303207686821339f4a0202f59
Joseph Vega <1josephvega@gmail.com>
2018-03-15 00:54:21 -0400

    Buttons adding and user input successfully pushing to topics array

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Function to house and run my code when the page loads
 $(document).ready(function () {
 
-    // Global variables========================================================================================================
     // Empty array for giphys
     var topics = []
+    console.log(topics);
 
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?q=rainbow&limit=10&api_key=mhgoaULVsWSutziQOiWLctWPaNKBRVrl";
+    // Variable for putting user input into the queryURL
+    // var search = $(response.data).data("search");
+    // Replace trending in queryURL when ready: search?q=" + search + "
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?q=&limit=10&api_key=mhgoaULVsWSutziQOiWLctWPaNKBRVrl";
+    
     console.log(queryURL);
 
     // AJAX function
@@ -44,4 +48,48 @@ $(document).ready(function () {
             displayDiv.append(displayRating);
         }
     });
+
+    // Function to iterate through topics array to create and append buttons to html
+    function displayButtons() {
+        
+        // Empty buttons div so that buttons don't repeat when another is added
+        $("#buttons").empty();
+        
+        // For loop to display buttons
+        for (var i = 0; i < topics.length; i++) {
+            
+            // Create button in html
+            var button = $("<button>");
+
+            // Add class and data attributes for putting user input into queryURL and pausing / unpausing later
+            button.addClass("btn");
+            button.attr("data-search", topics[i]);
+            button.text(topics[i]);
+
+            // Write buttons to html
+            $("#buttons").append(button);
+        }
+    }
+
+    // Function to add click event to the submit button that pushes userInput to topics array and makes a button
+    $("#submit").on("click", function(event) {
+        
+        // Don't let the user create a blank button
+        event.preventDefault();
+
+        // Variable for new buttons
+        var newButton = $("#userInput").val().trim();
+
+        // Push to topics array
+        topics.push(newButton);
+        console.log(topics);
+
+        // Clear userInput so they can enter new info
+        $("#userInput").val('');
+
+        // Rerun displayButtons to show newButton
+        displayButtons();
+    })
+
+
 });
